e3e7e185243f9c57b20261dc8cb138ea49e9a308
LeaHaelin <lea8haelin@gmail.com>
2023-03-16 17:48:03 +0100

    refactor form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-
+import React from "react";
+import "./styles/app.css";
+import { Nav } from "./components/Nav";
+import { Hero } from "./components/Hero";
+import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
+import iconGithub from "./assets/images/icon-github.svg";
+import iconFrontendM from "./assets/images/icon-frontend-mentor.svg";
+import iconLinkedin from "./assets/images/icon-linkedin.svg";
+import iconTwitter from "./assets/images/icon-twitter.svg";
 function App() {
   return (
     <div className="App">
+      <Nav />
+      <Hero />
+      <hr />
+      <Skills />
+      <hr />
+      <Projects />
+      <section className="footer">
+        <Contact />
+        <hr />
+        <Nav />
+      </section>
     </div>
   );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import profile from "../assets/images/profile-lea.png";
+
+export const Hero = () => {
+    return (
+        <div>Hero</div>
+    )
+}
