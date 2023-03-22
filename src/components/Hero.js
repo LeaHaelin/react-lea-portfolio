3e2d4f63f712a4ab99a2eef6771ae0e6407f3504
LeaHaelin <lea8haelin@gmail.com>
2023-03-22 20:52:14 +0100

    refactor styles

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,10 +4,12 @@ import profile from "../assets/images/profile-lea.png";
 export const Hero = () => {
     return (
         <div className='hero'>
-            <div className="hero__profile"></div>
+            <div className="hero__profile--bg">
+                <img className="hero__profile" src={profile} alt="profile" />
+            </div>
             <div className="hero__content">
                 <h1 className="hero__intro">Nice to meet you! I’m <span className="hero__intro--name">Lea Haelin Kim</span>.</h1>
-                <p className="hero__text">Based in the UK, I’m a front-end developer passionate about building accessible web apps that users love.</p>
+                <p className="hero__text">Based in Barcelona, Spain. I’m a front-end developer passionate about building accessible web apps that users love.</p>
                 <button className="hero__contact contact-me">contact me</button>
             </div>
         </div >
